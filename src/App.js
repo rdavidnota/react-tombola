99487e6d3307de16ced171b04748bc362cb5d471
rnota <rnota@gruposion.bo>
2021-12-18 16:15:44 -0400

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import MyCarousel from "./componetes/MyCarousel/MyCarousel.js";
 function App() {
   return (
     <Contenedor className="App">
+      <Titulo>¡Comencemos el juego!</Titulo>
       <MyCarousel></MyCarousel>
     </Contenedor>
   );
 }
-
+const Titulo = styled.h3`
+  
+element.style {
+  color: white;
+  font-weight: bold;
+  align-items: center;
+  text-align: center;
+  border-bottom: 1px solid white;
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+`;
 const Contenedor = styled.div`
   min-width: 100vw;
   min-height: 100vh;
-  padding-top: 20%;
+  padding-top: 15%;
   padding-left: 25%;
   padding-right: 25%;
   justify-content: "center";
